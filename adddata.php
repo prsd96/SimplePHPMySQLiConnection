3293d296c310ff343016c2f90f62fef7a5b48a73
prsd96 <prasadirache96@gmail.com>
2019-12-19 17:19:34 +0530

    frontend done using bootstrap.

--- adddata.php
+++ adddata.php
@@ -1,31 +1,62 @@
 <!DOCTYPE html>
 <html>
 <title>Record Entries</title>
+<link rel="stylesheet" type="text/css" href="stylecode.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <head>
 </head>
 <body>
 
-	<form action="data.php" method="post">
+	<div class="container" style="width: 60%">
+		<br><a href="index.html"><button type="button" class="btn btn-primary active">Go back to Home Page</button></a>	<br>
+		
+		<h2>Form to add data</h2>
+		
+		<form action="data.php" method="post">
+			
+			<div class="form-group">
+				<label>Name:</label>
+				<input type="text" name="name" class="form-control" placeholder="Enter your name" >
+			</div>
 
-		Name: <input type="text" name="name"><br><br>
-		Age: <input type="text" name="age"><br><br>
-		Gender: 
-		<select name="gender">
-			<option value=" ">Select Gender</option>
-			<option value="male">Male</option>
-			<option value="female">Female</option>
-		</select><br><br>
-		Department: 
-		<select name="department">
-			<option value=" ">Select Department</option>
-			<option value="mech">MECH</option>
-			<option value="etc">ETC</option>
-			<option value="comp">COMP</option>
-			<option value="it">IT</option>
-		</select><br><br>
+			
+			<div class="form-group">
+				<label>Age:</label>
+				<input type="text" name="age" class="form-control" placeholder="Enter your age">
+			</div>
 
-		<button type="submit">Add Data</button>
-	</form>
+			<div>
+				<div class="form-group" id="leftgndr">
+				<label>Gender:</label>
+				<select class="form-control" name="gender">
+					<option value=" ">Select Your Gender</option>
+					<option value="Male">Male</option>
+					<option value="Female">Female</option>
+				</select> 
+			</div>
+
+			<div class="form-group" id="rightdept">
+				<label>Department:</label>
+				<select class="form-control" name="department">
+					<option value=" ">Select Your Department</option>
+					<option value="MECH">MECH</option>
+					<option value="ETC">ETC</option>
+					<option value="COMP">COMP</option>
+					<option value="IT">IT</option>
+				</select> 
+			</div>
+			</div>
+
+			
+
+			<div class="form-group">
+				<center><button type="submit" class="btn btn-success">Add Data</button></center>
+			</div>
+		</form>
+	</div>
 
 </body>
 </html>
--- showdata.php
+++ showdata.php
@@ -9,56 +9,60 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<a href="index.html">Go back to Home Page</a>
-	
-	<table>
-		<center>
-			<tr>
-				<th>Name</th>
-				<th>Age</th>
-				<th>Gender</th>
-				<th>Department</th>
-			</tr>
+	<br>
+	<a href="index.html"><button type="button" class="btn btn-primary active">Go back to Home Page</button></a>
+	<a href="adddata.php"><button type="button" class="btn btn-primary active">Add More Data</button></a><br><br>
 
+	<div class="table-responsive">
 
-			
+		<center><table style="width: 80%" class="table table-bordered table-hover text-center">
+			<thead>
+				<tr class="success text-center">
+					<th><center>Name</center></th>
+					<th><center>Age</center></th>
+					<th><center>Gender</center></th>
+					<th><center>Department</center></th>
+				</tr>
+			</thead>
+				
 
+			<tbody>
 
+				<?php 
 
-			<?php 
+				//database connection
+				require('dbcon.php');
 
-//database connection
-			require('dbcon.php');
-
-//check connection
-			if ($conn->connect_error)
-			{
-				die("Connection failed: ". $conn->connect_error);
-			}
-
-			$sql = "SELECT stdname,stdage,stdgender,stddept from record";
-			$result = $conn-> query($sql);
-
-
-			if ($result-> num_rows > 0)
-			{
-				while ($row = $result->fetch_assoc()) 
+				//check connection
+				if ($conn->connect_error)
 				{
-					echo "<tr><td>".$row["stdname"]."</td><td>".$row["stdage"]."</td><td>".$row["stdgender"]."</td><td>".$row["stddept"]."</td></tr>";
+					die("Connection failed: ". $conn->connect_error);
 				}
-				echo "</table>";
-			}
-			else
-			{
-				echo "No entries yet!!";
-			}
 
-			$conn->close();
+				$sql = "SELECT stdname,stdage,stdgender,stddept from record";
+				$result = $conn-> query($sql);
 
-			?>
 
-		</center>
-	</table>
+				if ($result-> num_rows > 0)
+				{
+					while ($row = $result->fetch_assoc()) 
+					{
+						echo "<tr><td>".$row["stdname"]."</td><td>".$row["stdage"]."</td><td>".$row["stdgender"]."</td><td>".$row["stddept"]."</td></tr>";
+					}
+					echo "</table>";
+				}
+				else
+				{
+					echo "No entries yet!!";
+				}
+
+				$conn->close();
+
+				?>
+
+			</tbody>
+		</table></center>
+	</div>
 
 </body>
 </html>
