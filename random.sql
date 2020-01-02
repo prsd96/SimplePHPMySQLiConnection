2afe5c67a840cd80ba6ec8960020dff8ea70622d
Prasad Irache <58035379+prsd96@users.noreply.github.com>
2020-01-02 22:44:22 +0530

    Add files via upload

---
+++ random.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 02, 2020 at 06:07 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `random`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `record`
+--
+
+CREATE TABLE `record` (
+  `stdname` text DEFAULT NULL,
+  `stdage` int(100) DEFAULT NULL,
+  `stdgender` text DEFAULT NULL,
+  `stddept` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `record`
+--
+
+INSERT INTO `record` (`stdname`, `stdage`, `stdgender`, `stddept`) VALUES
+('Prasad', 23, 'Male', 'IT'),
+('Vedang', 25, 'Male', 'MECH'),
+('Omshree', 24, 'Female', 'ETC'),
+('Manjili', 23, 'Female', 'ETC'),
+('Ankita', 22, 'Female', 'IT'),
+('Ajit', 24, 'Male', 'MECH'),
+('Nolan', 24, 'Male', 'COMP'),
+('Zarina', 24, 'Female', 'COMP'),
+('paul', 24, 'Male', 'ETC');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
